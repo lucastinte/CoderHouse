068e65598a9b695e5a903a17e9cf269b90e85d9d
lepzito <lucasrtinte19@gmail.com>
2024-02-09 00:47:07 -0300

    listo

--- src/index.js
+++ src/index.js
@@ -1,91 +1,15 @@
 import express from "express";
-import { ProductManager } from "./config/ProductManager.js";
+import productsRouter from "./routes/productsRouter.js";
+import { __dirname } from "./path.js";
+import upload from "./config/multer.js";
+console.log(__dirname);
 const app = express();
 const port = 3000;
-const productManager = new ProductManager("./src/data/products.json");
+
 app.use(express.json())
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/static",express.static(__dirname + "/public"))
+app.use("/products",productsRouter)
 
-app.get("/products", async (req, res) => {
-  try {
-  const { limit } = req.query;
-  const prods = await productManager.getProducts();
-  const limite = parseInt(limit);
-  if (!isNaN(limite) && limite >= 0) {
-    const prodsLimit = prods.slice(0, limite);
-    res.send(prodsLimit);
-  } else if (!limit) {
-    res.send(prods);
-  } else {
-    res.status(400).send("Error al consultar productos, Ingrese un valor valido");
-  }
-} catch (error) {
- res.status(500).send(`Error interno del servidor al consultar productos:${error}`)   
-}
-});
-
-
-app.get("/products/:pid", async (req, res) => {
-  try {
-  const idProducto = req.params.pid;
-  const prod = await productManager.getProductById(idProducto);
-  if (prod) {
-    
-    res.status(200).send(prod);
-  } else {
-    res.status(404).send("Producto no existe")
-  }
-    
-} catch (error) {
-  res.status(500).send(`Error interno del servidor al consultar producto:${error}`)     
-}
-});
-app.post("/products", async (req, res) => {
-  try {
-    const newProduct=req.body
-   const mensaje = await productManager.addProduct(newProduct);
-  if (mensaje=="Product successfully added.") {
-    res.status(200).send(mensaje);
-  } else {
-    res.status(400).send(mensaje)
-  }
-    
-} catch (error) {
-  res.status(500).send(`Error interno del servidor al consultar producto:${error}`)     
-}
-});
-
-app.put("/products/:pid", async (req, res) => {
-  try {
-  const idProducto = req.params.pid;
-  const updatedProduct=req.body
-   const mensaje = await productManager.updateProduct(idProducto,updatedProduct);
-  if (mensaje=="Product successfully updated.") {
-    res.status(200).send(mensaje);
-  } else {
-    res.status(404).send(mensaje)
-  }
-    
-} catch (error) {
-  res.status(500).send(`Error interno del servidor al consultar producto:${error}`)     
-}
-});
-app.delete("/products/:pid", async (req, res) => {
-  try {
-  const idProducto = req.params.pid;
-   const mensaje = await productManager.deleteProduct(idProducto);
-  if (mensaje=="Product successfully deleted.") {
-    res.status(200).send(mensaje);
-  } else {
-    res.status(404).send(mensaje)
-  }
-    
-} catch (error) {
-  res.status(500).send(`Error interno del servidor al consultar producto:${error}`)     
-}
-});
 app.listen(port, () => {
   console.log("server on port " + port);
 });
