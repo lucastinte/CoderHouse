59dc410b3b28ea1c2074c4dd7d88768fd366172e
lepzito <lucasrtinte19@gmail.com>
2024-02-15 11:26:48 -0300

    clase10

--- src/index.js
+++ src/index.js
@@ -1,14 +1,39 @@
 import express from "express";
 import productsRouter from "./routes/productsRouter.js";
 import cartRouter from "./routes/cartRouter.js";
+import { Server } from "socket.io";
+import { engine } from "express-handlebars";
 import { __dirname } from "./path.js";
 import upload from "./config/multer.js";
 console.log(__dirname);
 const app = express();
 const port = 8080;
-
+//Server
+const server = app.listen(port, () => {
+  console.log("server on port " + port);
+});
+const io = new Server(server);
 app.use(express.json());
 app.use("/static", express.static(__dirname + "/public"));
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", __dirname + "/views");
+io.on("connection", (socket) => {
+  console.log("Conexion con Socket.io");
+
+  socket.on("movimiento", (info) => {
+    //Cuando el cliente me envia un mensaje, lo capturo y lo muestro
+    console.log(info);
+  });
+
+  socket.on("rendirse", (info) => {
+    //Cuando el cliente me envia un mensaje, lo capturo y lo muestro
+    console.log(info);
+    socket.emit("mensaje-jugador", "Te has rendido"); //Cliente que envio este mensaje
+    socket.broadcast.emit("rendicion", "El jugador se rindio"); //Clientes que tengan establecida la comunicacion con el servidor
+  });
+});
+
 app.use("/api/products", productsRouter);
 app.use("/api/cart", cartRouter);
 app.post("/upload", upload.single("product"), (req, res) => {
@@ -21,6 +46,37 @@ app.post("/upload", upload.single("product"), (req, res) => {
     res.status(500).send("imagen nop");
   }
 });
-app.listen(port, () => {
-  console.log("server on port " + port);
+//funciona dinamicamente pueo enviar la plantilla a renderizar y su estilo
+app.get("/static", (req, res) => {
+  const prods = [
+    {
+      id: 1,
+      title: "celular",
+      price: 1500,
+      img: "https://intercompras.com/product_thumb.php?img=images/product/VORAGO_CELL-500-PL.jpg&w=380&h=320",
+    },
+    {
+      id: 2,
+      title: "tele",
+      price: 1800,
+      img: "https://intercompras.com/product_thumb.php?img=images/product/VORAGO_CELL-500-PL.jpg&w=380&h=320",
+    },
+    {
+      id: 3,
+      title: "tablet",
+      price: 1200,
+      img: "https://intercompras.com/product_thumb.php?img=images/product/VORAGO_CELL-500-PL.jpg&w=380&h=320",
+    },
+    {
+      id: 4,
+      title: "reloj",
+      price: 16500,
+      img: "https://intercompras.com/product_thumb.php?img=images/product/VORAGO_CELL-500-PL.jpg&w=380&h=320",
+    },
+  ];
+  res.render("productos", {
+    mostrarProductos: true,
+    productos: prods,
+    css: "home.css",
+  });
 });
