ff5f95a90a7f4924bb25316823a90b4200016792
unknown <lucasrtinte19@gmail.com@>
2024-03-17 03:51:08 -0300

    segunda preentrega

--- src/routes/cartRouter.js
+++ src/routes/cartRouter.js
@@ -60,4 +60,110 @@ cartRouter.post("/:cid/:pid", async (req, res) => {
   }
 });
 
+//deberá eliminar del carrito el producto seleccionado.
+cartRouter.delete("/:cid/products/:pid", async (req, res) => {
+  try {
+    const cartId = req.params.cid;
+    const productId = req.params.pid;
+
+    const updatedCart = await cartModel.findOneAndUpdate(
+      { _id: cartId },
+      { $pull: { products: { id_prod: productId } } }, //eliminamos el producto del array segun el id
+      { new: true } //devolvemos el carrito actualizado
+    );
+
+    if (updatedCart) {
+      res.status(200).send(updatedCart);
+    } else {
+      res.status(404).send("Cart not found");
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .send(`internal error when deleting product from cart: ${error}`);
+  }
+});
+//EXAMPLE JSON NEW ARRAY FOR CART
+// [
+//   {
+//     "id_prod": "65f41885758419b117b9f787",
+//     "quantity": 2
+//   },
+//   {
+//     "id_prod": "65f41834758419b117b9f785",
+//     "quantity": 3
+//   },
+//   {
+//     "id_prod": "65f41854758419b117b9f786",
+//     "quantity": 1
+//   }
+// ]
+
+cartRouter.put("/:cid", async (req, res) => {
+  try {
+    const cartId = req.params.cid;
+    const newProducts = req.body;
+    const updatedCart = await cartModel.findOneAndUpdate(
+      { _id: cartId },
+      { $set: { products: newProducts } },
+      { new: true }
+    );
+    if (!updatedCart) {
+      return res.status(404).send("Cart not found");
+    }
+
+    res.status(200).send(updatedCart);
+  } catch (error) {
+    res
+      .status(500)
+      .send(`internal error when updating products from cart: ${error}`);
+  }
+});
+//Aqui actualiza la cantidad de productos mediante un set pero no los suma
+cartRouter.put("/:cid/products/:pid", async (req, res) => {
+  try {
+    const cartId = req.params.cid;
+    const productId = req.params.pid;
+    const { quantity } = req.body;
+
+    if (isNaN(quantity) || quantity < 0) {
+      return res.status(400).send("Invalid quantity");
+    }
+
+    const updatedCart = await cartModel.findOneAndUpdate(
+      { _id: cartId, "products.id_prod": productId },
+      { $set: { "products.$.quantity": quantity } },
+      { new: true }
+    );
+
+    if (!updatedCart) {
+      return res.status(404).send("Cart or product not found");
+    }
+
+    res.status(200).send(updatedCart);
+  } catch (error) {
+    res.status(500).send(`Internal error : ${error}`);
+  }
+});
+//deja solo un array vacio en el carrito []
+cartRouter.delete("/:cid", async (req, res) => {
+  try {
+    const cartId = req.params.cid;
+
+    const updatedCart = await cartModel.findByIdAndUpdate(
+      cartId,
+      { products: [] },
+      { new: true }
+    );
+
+    if (!updatedCart) {
+      return res.status(404).send("Cart not found");
+    }
+
+    res.status(200).send(" removed!");
+  } catch (error) {
+    res.status(500).send(`Internal error : ${error}`);
+  }
+});
+
 export default cartRouter;
